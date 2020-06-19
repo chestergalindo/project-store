d9366598bd24a345b38b4fcce95e6bd163ed2cf4
Bryson Florez <brysonsaman@hotmail.com>
2020-06-19 16:52:47 -0500

    My first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+export const Home = () => {
+  const view = `
+    <div class="Items">
+      <article class="Item">
+        <a href="#/1">
+          <h2>Name</h2>
+        </a>
+      </article>
+    </div>
+  `;
+
+  return view;
+}
